eb73dfb4d0f92ae766288ef0206edeca48e39e87
Payton <psides83@hotmail.com>
2022-06-10 06:24:23 -0500

    firebase function update

--- functions/index.js
+++ functions/index.js
@@ -9,18 +9,18 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Leads!");
 });
 
-exports.calculateSalesData = functions.firestore
-  .document("soldEquipment/{soldEquipmentId}")
-  .onWrite((change, context) => {
-    const data = change.after.data();
-    const previousData = change.before.data();
-    const commission = data.margin * 0.12;
+// exports.calculateSalesData = functions.firestore
+//   .document("soldEquipment/{soldEquipmentId}")
+//   .onWrite((change, context) => {
+//     const data = change.after.data();
+//     const previousData = change.before.data();
+//     const commission = data.margin * 0.12;
 
-    if (data.companyName == previousData.companyName) {
-      return null;
-    }
+//     if (data.companyName == previousData.companyName) {
+//       return null;
+//     }
 
-    return change.after.ref.set({ commission: commission }, { merge: true });
-  });
+//     return change.after.ref.set({ commission: commission }, { merge: true });
+//   });
 
   
