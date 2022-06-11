401a186dbb1827e9ff45846fb4c95fb1c55e068e
Payton Sides <psides83@hotmail.com>
2022-06-11 12:53:30 -0500

    more data features added

--- src/components/sales-components/sales-data-grid.js
+++ src/components/sales-components/sales-data-grid.js
@@ -11,7 +11,11 @@ import { db } from "../../services/firebase";
 import { currencyFormatter } from "../../utils/utils";
 import moment from "moment";
 import { Box, Button, Stack, Typography } from "@mui/material";
-import { Money } from "@mui/icons-material";
+import {
+  ArrowDownwardRounded,
+  ArrowUpwardRounded,
+  Money,
+} from "@mui/icons-material";
 
 const columns = [
   {
@@ -80,7 +84,7 @@ export default function SalesDataGrid() {
   const [sales, setSales] = useState([]);
   const [filterParam, setFilterParam] = useState("All");
   const [salesData, setSalesData] = useState({});
-  const dataTypes = ["Sales", "Margin", "Commission"]
+  const dataTypes = ["Gross Revenue", "Margin", "Commission", "Bonus"];
   const years = [
     moment().subtract(3, "years").format("yyyy"),
     moment().subtract(2, "years").format("yyyy"),
@@ -105,7 +109,7 @@ export default function SalesDataGrid() {
   //    Fetch leads from firestore
   const fetchSales = useCallback(async () => {
     const salesQuery = query(
-      collection(db, "soldEquipment"),
+      collection(db, "salesDataByMonth"),
       orderBy("id", "asc")
     );
 
@@ -113,19 +117,13 @@ export default function SalesDataGrid() {
       setSales(
         querySnapshot.docs.map((doc) => ({
           id: doc.data().id,
-          saleID: doc.data().saleID,
-          saleDate: doc.data().saleDate,
-          saleMonth: doc.data().saleMonth,
-          saleYear: doc.data().saleYear,
-          salePrice: Number(doc.data().salePrice),
+          date: doc.data().date,
+          month: doc.data().month,
+          year: doc.data().year,
+          sales: Number(doc.data().sales),
           margin: Number(doc.data().margin),
-          //   commission: commission,
-          tradeAttached: doc.data().tradeAttached,
-          tradeSold: doc.data().tradeSold,
-          dateTradeSold: doc.data().dateTradeSold,
-          tradeSoldMonth: doc.data().tradeSoldMonth,
-          tradeSoldYear: doc.data().tradeSoldYear,
-          model: doc.data().model,
+          commission: Number(doc.data().commission),
+          bonus: Number(doc.data().bonus),
         }))
       );
     });
@@ -134,41 +132,120 @@ export default function SalesDataGrid() {
   const calculateSales = (type, year) => {
     var salesDollars = 0;
     var marginDollars = 0;
+    var commission = 0;
+    var bonus = 0;
     // console.log(year);
     // console.log(type);
 
     sales
       ?.filter((sale) => {
-
-        if (
-          (!sale.tradeAttached || sale.dateTradeSold !== "") &&
-          (sale.saleYear === year || sale.tradeSoldYear === year)
-        ) {
+        if (sale.year === year) {
           return sale;
         }
       })
       .forEach((value) => {
-        salesDollars += value.salePrice;
+        salesDollars += value.sales;
         marginDollars += value.margin;
+        commission += value.commission;
+        bonus += value.bonus;
       });
 
-    if (type === "Sales") return salesDollars;
-    console.log(salesDollars)
+    if (type === "Gross Revenue") return salesDollars;
 
     if (type === "Margin") return marginDollars;
 
-    if (type === "Commission")
-      return marginDollars * 0.12;
+    if (type === "Commission") return commission;
+
+    if (type === "Bonus") return bonus;
   };
 
   const barHeight = (type, year) => {
-    if (type === "Sales") return calculateSales(type, year) / 20000
-    if (type === "Margin") return calculateSales(type, year) / 1500
-    if (type === "Commission") return calculateSales(type, year) / 200
-  }
+    if (type === "Gross Revenue") return calculateSales(type, year) / 20000;
+    if (type === "Margin") return calculateSales(type, year) / 1500;
+    if (type === "Commission") return calculateSales(type, year) / 200;
+    if (type === "Bonus") return calculateSales(type, year) / 200;
+  };
+
+  const currentVsPreviousYearToDate = (type) => {
+    const currentYear = moment().format("yyyy");
+    const previousYear = moment().subtract(1, "years").format("yyyy");
+    const currentMonth = moment().format("MM");
+
+    var currentYearTotal = 0;
+    var previousYearToDate = 0;
+
+    sales
+      ?.filter((sale) => {
+        if (sale.year === currentYear) return sale;
+      })
+      .forEach((value) => {
+        if (type === "Gross Revenue") currentYearTotal += value.sales;
+        if (type === "Margin") currentYearTotal += value.margin;
+        if (type === "Commission") currentYearTotal += value.commission;
+        if (type === "Bonus") currentYearTotal += value.bonus;
+        
+      });
+
+    sales
+      ?.filter((sale) => {
+        if (
+          sale.year === previousYear &&
+          sale.month >= "01" &&
+          sale.month <= currentMonth
+        )
+          return sale;
+      })
+      .forEach((value) => {
+        if (type === "Gross Revenue") previousYearToDate += value.sales;
+        if (type === "Margin") previousYearToDate += value.margin;
+        if (type === "Commission") previousYearToDate += value.commission;
+        if (type === "Bonus") previousYearToDate += value.bonus;
+      });
+
+    const difference = currentYearTotal / previousYearToDate - 1;
+
+    return difference;
+  };
+
+  const trend = (type) => {
+    if (currentVsPreviousYearToDate(type) === 0) {
+      return (
+        <Typography>
+          <strong>the same</strong> as last year
+        </Typography>
+      );
+    }
+    if (currentVsPreviousYearToDate(type) < 0) {
+      return (
+        <Stack direction="row">
+          <ArrowDownwardRounded color="error" alignSelf="flex-end" />
+          <Typography>
+            <strong>
+              {Math.abs(Math.round(currentVsPreviousYearToDate(type) * 100))}%
+            </strong>{" "}
+            from last year
+          </Typography>
+        </Stack>
+      );
+    }
+    if (currentVsPreviousYearToDate(type) > 0) {
+      return (
+        <Stack direction="row">
+          <ArrowUpwardRounded color="error" alignSelf="flex-end" />
+          <Typography>
+            <strong>
+              {Math.abs(Math.round(currentVsPreviousYearToDate(type) * 100))}%
+            </strong>{" "}
+            from last year
+          </Typography>
+        </Stack>
+      );
+    }
+  };
 
   useEffect(() => {
     fetchSales();
+    console.table(sales);
   }, [fetchSales]);
 
   return (
@@ -193,28 +270,67 @@ export default function SalesDataGrid() {
           // border: "solid black 2px",
           borderRadius: "10px",
           alignItems: "center",
-          justifyContent: "space-between"
+          justifyContent: "space-between",
         }}
       >
-
         {dataTypes.map((type) => (
-          <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", maxWidth: '500px'}} >
-            <Typography variant="h5" color="primary" sx={{fontWeight: "medium"}} alignSelf="center">{type}</Typography>
-          <Stack direction="row" alignItems="flex-end" justifyContent="space-around" sx={{border: "solid #FFDE00 2px"}}>
-            {years.map((year) => (
-              <Stack direction="column" justifyContent="center" alignItems="center" sx={{margin: "10px"}}>
+          <Box
+            key={type}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              maxWidth: "500px",
+            }}
+          >
+            <Typography>
+              Your <strong>{type}</strong> is
+            </Typography>
 
-                <Typography>{year}</Typography>
-                <Box sx={{width: "20px", height: barHeight(type, year), backgroundColor: "#367C2B", borderRadius: "3px 3px 0 0"}}/>
-                <Typography>{currencyFormatter.format(calculateSales(type, year))}</Typography>
-              </Stack>
-            ))}
-            {/* <Typography>Sales: {currencyFormatter.format(calculateSales(salesTotal, year))}</Typography>
+            {trend(type)}
+            <Typography
+              variant="h5"
+              color="primary"
+              sx={{ fontWeight: "medium" }}
+              alignSelf="center"
+            >
+              {type}
+            </Typography>
+            <Stack
+              direction="row"
+              alignItems="flex-end"
+              justifyContent="space-around"
+              sx={{ border: "solid #FFDE00 2px" }}
+            >
+              {years.map((year) => (
+                <Stack
+                  key={year}
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{ margin: "10px" }}
+                >
+                  <Typography>{year}</Typography>
+                  <Box
+                    sx={{
+                      width: "20px",
+                      height: barHeight(type, year),
+                      backgroundColor: "#367C2B",
+                      borderRadius: "3px 3px 0 0",
+                    }}
+                  />
+                  <Typography>
+                    {currencyFormatter.format(calculateSales(type, year))}
+                  </Typography>
+                </Stack>
+              ))}
+              {/* <Typography>Sales: {currencyFormatter.format(calculateSales(salesTotal, year))}</Typography>
             <Typography>Margin: {currencyFormatter.format(calculateSales(marginTotal, year))}</Typography>
             <Typography>
               Commission: {currencyFormatter.format(calculateSales(commissionTotal, year))}
             </Typography> */}
-          </Stack>
+            </Stack>
           </Box>
         ))}
       </Box>
